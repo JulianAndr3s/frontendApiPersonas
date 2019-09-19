99cd4bcec6a00f1467f566bb747733f92badf2d6
Julián Botero <juanbo97@hotmail.com>
2019-09-19 15:09:12 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/partes/home/home.component';
+import { ListaPersonaComponent } from './components/partes/lista-persona/lista-persona.component';
+import { FormularioPersonaComponent } from './components/partes/formulario-persona/formulario-persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ListaPersonaComponent,
+    FormularioPersonaComponent
   ],
   imports: [
     BrowserModule,
