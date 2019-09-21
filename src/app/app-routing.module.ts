8403ebb0187b8454f60e8ba369b1fda79517fd2f
Julián Botero <juanbo97@hotmail.com>
2019-09-21 12:15:46 -0500

    Parametrizando el Angular Routing
    Se añadió al App-Routing.module.ts las rutas de todas las páginas a usar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/partes/home/home.component';
+import { FormularioPersonaComponent } from './components/partes/formulario-persona/formulario-persona.component';
+import { ListaPersonaComponent } from './components/partes/lista-persona/lista-persona.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'lista-persona', component: ListaPersonaComponent},
+  {path: 'formulario-persona', component: FormularioPersonaComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
