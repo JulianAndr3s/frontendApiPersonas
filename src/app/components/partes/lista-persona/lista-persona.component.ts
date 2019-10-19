a1bd7548287004f98eef222c4e36abb789ebb077
Julián Botero <juanbo97@hotmail.com>
2019-10-19 15:30:55 -0500

    Implementación del Actualizar
    Se termina el Update.

--- src/app/components/partes/lista-persona/lista-persona.component.ts
+++ src/app/components/partes/lista-persona/lista-persona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { Persona } from 'src/app/modelo/persona';
 import { PersonaService } from 'src/app/service/persona.service';
@@ -14,12 +15,23 @@ export class ListaPersonaComponent implements OnInit {
 
   public persona: Persona = new Persona();
   public personaSeleccionada: Persona;
+  public personaActualizar: Persona = new Persona();
   public documento: Tipodocumento = new Tipodocumento();
   public personas: Persona[];
   public documentos: Tipodocumento[];
-  constructor(private documentoService: TipodocumentoService, private personaService: PersonaService) { }
+  public idDocumento: number;
+  constructor(private documentoService: TipodocumentoService, private personaService: PersonaService, private router: Router) { }
 
   ngOnInit() {
+    this.listAllComponents();
+  }
+
+  listAllComponents() {
+    this.documentoService.listAllDocumentos().subscribe(
+      (documento) => {
+        this.documentos = documento;
+      }
+    );
     this.personaService.listAll().subscribe(
       (persona) => {
         this.personas = persona;
@@ -36,6 +48,16 @@ export class ListaPersonaComponent implements OnInit {
       _ => (
         this.personas = this.personas.filter(persona => persona !== this.personaSeleccionada)
       )
+    );
+  }
+
+  actualizar(idActualizar: number) {
+    this.documento.id = this.idDocumento;
+    this.personaActualizar.tipoDocumento = this.documento;
+    this.personaService.actualizar(this.personaActualizar, idActualizar).subscribe(
+      _ => {
+        this.router.navigate(['/home']);
+      }
     );
   }
 
